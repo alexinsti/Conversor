d700d746d7fe39c7a7b53bfdcfc5b045a66a2b8f
Alejandro Alvarez Salas <damm@192.168.208.170>
2022-10-19 14:43:00 +0200

    conversiones hasta masa

--- src/conversor/Conversor.java
+++ src/conversor/Conversor.java
@@ -4,7 +4,14 @@
  */
 package conversor;
 
+/*
+Millas a KM
+Litros a Galones
+Kilos a Libras
+Dolares a Euros
+Metros a Pies
 
+*/
 
 /**
  *
@@ -12,6 +19,41 @@ package conversor;
  */
 public class Conversor {
     
+    public static double kmAMillas(double km){
+        double resultado;
+        resultado=km*1.60934;
+        return resultado;
+    }
+    
+    public static double millasAKm(double millas){
+        double resultado;
+        resultado=millas*0.621371;
+        return resultado;
+    }
+    
+    public static double lAGalones(double l){
+        double resultado;
+        resultado=l*0.219969;
+        return resultado;
+    }
+    
+    public static double galonesAL(double galones){
+        double resultado;
+        resultado=galones*4.54609;
+        return resultado;
+    }
+    
+    public static double kgAlibras(double kg){
+        double resultado;
+        resultado=kg*2.20462;
+        return resultado;
+    }
+    
+    public static double librasAKg(double libras){
+        double resultado;
+        resultado=libras*0.453592;
+        return resultado;
+    }
     
     
     
